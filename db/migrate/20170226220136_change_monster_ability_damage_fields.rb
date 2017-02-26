c175b4ff8d94fd872853a5feec35e44f4d3319b0
Evan Gillespie <evan.gillespie1@gmail.com>
2017-02-26 15:24:00 -0700

    Added a bused migration to change monster ability field types so that Jonny can work on it.

---
+++ db/migrate/20170226220136_change_monster_ability_damage_fields.rb
@@ -0,0 +1,16 @@
+class ChangeMonsterAbilityDamageFields < ActiveRecord::Migration[5.0]
+  def change
+    reversible do |dir|
+      change_table :monster_abilities do |t|
+        dir.up do
+          t.change :attack_bonus, 'integer USING CAST(column_name AS integer)'
+          t.change :damage_bonus, 'integer USING CAST(column_name AS integer)'
+        end
+        dir.down do
+          t.change :attack_bonus, :string
+          t.change :damage_bonus, :string
+        end
+      end
+    end
+  end
+end
