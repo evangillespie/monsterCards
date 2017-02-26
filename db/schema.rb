fe3ed06c85edb1443a5f295fd912951522ae6fa7
Jonny Johannes <jonny@jonnyjohannes.com>
2017-02-26 16:27:57 -0700

    Add lisence and title to monster_sets.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170226224626) do
+ActiveRecord::Schema.define(version: 20170226232630) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20170226224626) do
     t.text     "desc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "title"
+    t.string   "license"
   end
 
   create_table "monsters", force: :cascade do |t|
