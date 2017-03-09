f5ae33c2990a7ffb0d14716c1943ad6bead89420
Jonny Johannes <jonny@jonnyjohannes.com>
2017-03-08 21:15:29 -0700

    #2. Consume search query and match to monster name.

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,7 +1,11 @@
 class MonstersController < ApplicationController
 
   def index
-    @monsters = Monster.all
+    if params[:search]
+      @monsters = Monster.where("name like ?", '%' + params[:search] + '%')
+    else
+      @monsters = Monster.all
+    end
   end
 
   def show
