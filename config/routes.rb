2dad240473e7e17ffdc55269419137c66721f984
Evan Gillespie <evan.gillespie1@gmail.com>
2017-03-18 21:02:15 -0600

    Added a page with the license for 5e SRD monsters

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'monsters#index'
   resources :monsters, only: [:index, :show]
 
-  get "/pages/:page" => "pages#show"
+  get "/pages/*page" => "pages#show"
 end
