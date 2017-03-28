78070e2781acccbf04802bde224865cc2b0e37b3
Evan Gillespie <evan.gillespie1@gmail.com>
2017-03-27 17:31:31 -0700

    Changed all the page titles

--- app/controllers/monsters_controller.rb
+++ app/controllers/monsters_controller.rb
@@ -1,6 +1,7 @@
 class MonstersController < ApplicationController
 
   def index
+    @title = "Monsters - D&D 5e SRD"
     if params[:search]
       @monsters = Monster.where("name ilike ?", '%' + params[:search] + '%')
     else
@@ -12,5 +13,6 @@ class MonstersController < ApplicationController
     @monster = Monster.
       includes(:monster_abilities).
       find(params[:id])
+    @title = @monster.name + " - D&D 5e SRD"
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
 	def show
 		if valid_page?
+			@title = get_page_title(params[:page])
 			render template: "pages/#{params[:page]}"
 		else
 			render file: "public/404.html", status: :not_found
@@ -10,5 +11,16 @@ class PagesController < ApplicationController
 	private 
 	def valid_page?
 		File.exist?(Pathname.new(Rails.root + "app/views/pages/#{params[:page]}.html.erb"))
+	end
+	def get_page_title path
+		custom_names = {
+			"license/5e_SRD" => "D&D 5e SRD License" 
+		}
+
+		if custom_names.keys.include?(path)
+			return custom_names[path]
+		else
+			return path.titleize
+		end
 	end
 end
