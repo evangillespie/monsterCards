08fdca9b11db894590f8c0866012990bdbd3072b
Colin Sproule <colinsproule@gmail.com>
2017-09-30 14:02:01 -0600

    #28 adding an empty state to the favourites page to be rendered in the favourite

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,12 @@ Rails.application.routes.draw do
   get "favourites", to: "favourites#index"
   get "favourites/fetch", to: "favourites#fetch"
 
+  get ":page", to: "pages#show"
+
   resources :monster_sets, only: [:show], path: '' do
     get '/license', to: 'monster_sets#show'
     resources :monsters, only: [:index, :show], path: ''
   end
-
-  get ":page", to: "pages#show"
 
   get '*unmatched_route', to: 'application#not_found'
 
