d4e285c732ad13188768df655e6cbbce398f9e3c
Evan Gillespie <evan@Evans-MacBook-Pro.local>
2017-11-18 18:15:57 -0700

    Added a slider for CR filtering. It does nothing yet but it's ready for styling. #8

--- app/assets/javascripts/filters.js
+++ app/assets/javascripts/filters.js
@@ -15,7 +15,33 @@ var filters = (function($, window, document) {
   var initFilter = function() {
     var searchString = $('#search').val();
     monsterFilter(searchString);
+    initSliders();
   };
+
+  var initSliders = function() {
+    var crFilterSlider = document.getElementById('cr-filter-slider');
+
+    noUiSlider.create(crFilterSlider, {
+      start: [ 0, 15 ],
+      behaviour: 'tap',
+      connect: true,
+      range: {
+        'min':  0,
+        'max':  15
+      },
+      step: 1,
+      format: {
+        to: function(value){
+          return Math.round(value);
+        },
+        from: function(value){
+          return value;
+        }
+      }
+    });
+    // bind slider update events to the right function
+    crFilterSlider.noUiSlider.on('update', updateCrFilterSliderLabels);
+  }
 
   var initMonsterSearch = function() {
     $('#search').on('keyup', function() {
@@ -28,6 +54,11 @@ var filters = (function($, window, document) {
 
   var initMonsterSort = function() {
     config.monsterList.sort('monster-xp', { order: 'asc' });
+  }
+
+  var updateCrFilterSliderLabels = function(values, handle, unencoded, tap, positions){
+    var h = $('div[data-handle="'+handle+'"]');
+    h.html(values[handle]);
   }
 
   var monsterFilter = function(searchString) {
