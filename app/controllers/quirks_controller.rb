369995757a81a0ba6a4d6c9b47bebce7d2e38f4e
evangillespie <evan.gillespie1@gmail.com>
2020-01-11 15:27:12 -0700

    Added existing quirks for the current category into the quirks form page

--- app/controllers/quirks_controller.rb
+++ app/controllers/quirks_controller.rb
@@ -2,6 +2,10 @@ class QuirksController < ApplicationController
   
   def index
     @category = QuirkCategory.find(rand(QuirkCategory.count)+1)
+
+    @existing_quirks = Quirk.
+      where(quirk_category_id: @category.id).
+      order(:created_at)
   end
 
   def create
