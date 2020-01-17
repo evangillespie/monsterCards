6ccb370d76bc4ad1835fd40e09431a7b3e9afec2
evangillespie <evan.gillespie1@gmail.com>
2020-01-16 21:43:57 -0700

    Changed quirk category to use a random selection of the 25 least popular quir categories insted of a random selection from all categories

--- app/controllers/quirks_controller.rb
+++ app/controllers/quirks_controller.rb
@@ -1,7 +1,22 @@
 class QuirksController < ApplicationController
 
   def index
-    @category = QuirkCategory.find(rand(QuirkCategory.count)+1)
+    @category = QuirkCategory.find(
+      ActiveRecord::Base.connection.execute(
+        "
+        SELECT qc.*, COALESCE(counts.C, 0) AS quirk_count
+        FROM quirk_categories AS qc
+        LEFT OUTER JOIN (
+          SELECT quirk_category_id, COUNT(id) AS c
+          FROM quirks
+          GROUP BY quirk_category_id
+        ) AS counts ON qc.id = counts.quirk_category_id
+        ORDER BY quirk_count ASC
+        LIMIT 25
+        ;
+        "
+      ).field_values('id').sample
+    )
 
     @existing_quirks = Quirk.
       where(quirk_category_id: @category.id).
