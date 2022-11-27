d09bd27046943ccb7fece5c6ab35ca210b1b9103
Jonny Johannes <jonny@jonnyjohannes.com>
2022-11-27 13:13:03 -0500

    restore favourites page behaviour with client-side-only processing

--- app/controllers/favourites_controller.rb
+++ app/controllers/favourites_controller.rb
@@ -3,17 +3,4 @@ class FavouritesController < ApplicationController
   def index
   end
 
-  def fetch
-    @monsters = Monster.
-      where(slug: params['favourite_monsters']).
-      order(:name)
-    if @monsters.present?
-      render 'shared/_monsters',
-        :layout => false,
-        :locals => {:monsters => @monsters}
-    else
-      render '_empty_state', layout: false
-    end
-  end
-
 end
